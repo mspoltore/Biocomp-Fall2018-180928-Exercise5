05ee6686e8b132de254becb6fbe8db5edc466d3f
Michael Spoltore <michaelspoltore@michaels-mbp.dhcp.nd.edu>
2018-10-03 16:26:30 -0400

    fixed some spelling errors

--- exercise5.sh
+++ exercise5.sh
@@ -2,7 +2,7 @@
 
 # This part of the script will cat the info from the csv file, cut the first two fields
 # of a csv, then it will turn the commas to spaces, then only unique results are 
-# returned by sorting and using uniq, hten teh unique results are put in a file 
+# returned by sorting and using uniq, then the unique results are put in a file 
 
 cat $1 | cut -d , -f 1,2 | tr "," " " | sort | uniq > wagesSorted.txt
 
