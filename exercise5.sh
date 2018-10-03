2c8c8075b25a112dcad45feaab1a30fffe2a81ba
Michael Spoltore <michaelspoltore@michaels-mbp.dhcp.nd.edu>
2018-10-03 16:59:37 -0400

    added some work for part 2

--- exercise5.sh
+++ exercise5.sh
@@ -1,8 +1,21 @@
 # Usage: bash exercise5.sh fileName.csv
 
-# This part of the script will cat the info from the csv file, cut the first two fields
-# of a csv, then it will turn the commas to spaces, then only unique results are 
-# returned by sorting and using uniq, then the unique results are put in a file 
+# Part 1
+# This part of the script will cat the info from the csv file, cut the first two
+# fields of a csv, then it will turn the commas to spaces, then only unique results
+# are returned by sorting and using uniq, then the unique results are put in a file 
 
 cat $1 | cut -d , -f 1,2 | tr "," " " | sort | uniq > wagesSorted.txt
+
+# This line should reverse sort the lines by wage, returning the person with the
+# highest wage to stdout
+
+echo "Highest Earner"
+cat $1 | sort -r -n -k 4 -t , | head -n 1
+
+# This line should sort the lines by wage and return the person with the lowest wage
+# to stdout (plus the column names)
+
+echo "Lowest Earner"
+cat $1 | sort -n -k 4 -t , | head -n 2
 
