cb93b727cf960fa4b5a88a75d1a4da771ad939e1
Michael Spoltore <michaelspoltore@michaels-mbp.dhcp.nd.edu>
2018-10-04 20:35:13 -0400

    finished the script

--- exercise5.sh
+++ exercise5.sh
@@ -25,3 +25,16 @@ cat $1 | sort -n -k 4 -t , | head -n 2
 
 echo "Number of Women in Top 10 of Wages"
 cat wages.csv | sort -r -n -k 4 -t , | head -n 10 | grep female | wc -l
+
+#Return one more piece of information to stdout:
+# the effect of graduating college (12 vs. 16 years of school) on the minimum wage for earners in this dataset.
+# Hint: you can define a shell variable with a numeric variable
+# and then use the command bc (“An arbitrary precision calculator language”). 
+#If you’ve defined two shell variables (val1 and val2) that are decimal values
+# you can subtract them with the following code: echo "$val1 - $val2" | bc
+
+for variable in {12, 16} 
+do 
+cat $1 | cut -n -d, -f 3,4 | grep var$variable | echo "var16 - var12" | bc 
+done 
+
